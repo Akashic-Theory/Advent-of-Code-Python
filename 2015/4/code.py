f99ef4c7fd03125c093f9ef793aad993c31d39a3
Akasha <akashic.theory@gmail.com>
2025-11-21 21:42:25 -0600

    :sparkles: [2015] Day 4 Complete

--- 2015/4/code.py
+++ 2015/4/code.py
@@ -1,13 +1,24 @@
 # Advent of code Year 2015 Day 4 solution
 # Author = Akasha Mohan
+import hashlib
 
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.read()
 
+# Part 1
+i=0
+while True:
+    if hashlib.md5((input+str(i)).encode("utf-8")).hexdigest()[:5] == "00000":
+        break
+    i+=1
 
+print("Part One : "+ str(i))
 
-print("Part One : "+ str(None))
+# Part 2
+i=0
+while True:
+    if hashlib.md5((input+str(i)).encode("utf-8")).hexdigest()[:6] == "000000":
+        break
+    i+=1
 
-
-
-print("Part Two : "+ str(None))
+print("Part Two : "+ str(i))
