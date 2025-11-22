7a0b676fc937643fdd2ea18ec709aebdbc04f94c
Akasha <akashic.theory@gmail.com>
2025-11-21 21:09:50 -0600

    :tada: Project Start with templates!

---
+++ 2017/18/code.py
@@ -0,0 +1,13 @@
+# Advent of code Year 2017 Day 18 solution
+# Author = Akasha Mohan
+
+with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
+    input = input_file.read()
+
+
+
+print("Part One : "+ str(None))
+
+
+
+print("Part Two : "+ str(None))
