a8a41a1a56ff622dda32d9ded343457d8aef7230
Akasha <akashic.theory@gmail.com>
2025-11-21 22:24:12 -0600

    :sparkles: [2015] Day 5 Complete

--- 2015/5/code.py
+++ 2015/5/code.py
@@ -1,13 +1,28 @@
 # Advent of code Year 2015 Day 5 solution
 # Author = Akasha Mohan
+import re
 
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.read()
 
+# Part 1
+nice = 0
+for line in input.splitlines():
+    if re.search(r"([aeiou].*){3,}", line) is None:
+        continue
+    if re.search(r"(?P<a>.)(?P=a)", line) is None:
+        continue
+    if re.search(r"(ab)|(cd)|(pq)|(xy)", line) is None:
+        nice += 1
+print("Part One : "+ str(nice))
 
+# Part 2
+nice = 0
+for line in input.splitlines():
+    if re.search(r"(?P<a>..).*(?P=a)", line) is None:
+        continue
+    if re.search(r"(?P<a>.).(?P=a)", line) is None:
+        continue
+    nice += 1
 
-print("Part One : "+ str(None))
-
-
-
-print("Part Two : "+ str(None))
+print("Part Two : "+ str(nice))
