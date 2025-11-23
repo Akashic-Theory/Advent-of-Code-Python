ee6bbc8df5dd987192b7a91b49dce31cdbee55c4
Akasha <akashic.theory@gmail.com>
2025-11-23 17:19:50 -0600

    :sparkles: [2015] Day 10 Complete

--- 2015/10/code.py
+++ 2015/10/code.py
@@ -4,10 +4,46 @@
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.read()
 
+# Part 1
 
 
-print("Part One : "+ str(None))
+for i in range(40):
+    count = 0
+    active = None
+    out=""
+    for char in input:
+        if char == active or active is None:
+            count += 1
+        else:
+            out += str(count) + active
+            active = char
+            count = 1
+        if active is None:
+            active = char
+    input = out + str(count) + active
+    print(f"\r{'#'*(i+1) + '.'*(39-i)}", end="", flush=True)
+print("\nPart One : "+ str(len(input)))
+
+# Part 2
+
+for i in range(10):
+    progress = 0
+    count = 0
+    active = None
+    out=""
+    l=len(input)
+    for char in input:
+        if char == active or active is None:
+            count += 1
+        else:
+            out += str(count) + active
+            active = char
+            progress += count
+            count = 1
+        if active is None:
+            active = char
+        print(f"\r{'#' * (i + 1) + '.' * (9 - i)}  ({progress}/{l})", end="", flush=True)
+    input = out + str(count) + active
 
 
-
-print("Part Two : "+ str(None))
+print("\nPart Two : "+ str(len(input)))
