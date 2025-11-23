12438dd871ef44eab2469e8e4b4cb297e8885ceb
Akasha <akashic.theory@gmail.com>
2025-11-23 02:53:09 -0600

    :sparkles: [2015] Day 8 Complete

--- 2015/08/code.py
+++ 2015/08/code.py
@@ -1,13 +1,17 @@
 # Advent of code Year 2015 Day 8 solution
 # Author = Akasha Mohan
+import codecs
 
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
     input = input_file.read()
+    
+# Part 1
+lines = [[line, eval(line)] for line in input.splitlines()]
+counts = [len(a) - len(b) for a, b in lines]
+print(counts)
+print("Part One : "+ str(sum(counts)))
 
-
-
-print("Part One : "+ str(None))
-
-
-
-print("Part Two : "+ str(None))
+# Part 2
+extras = [2 + line.count('\\') + line.count('"') for line in input.splitlines()]
+print(extras)
+print("Part Two : "+ str(sum(extras)))
